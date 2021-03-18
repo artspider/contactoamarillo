4a0a62dbe3bf19f4ac34c9d906bf2b90cb6cc659
artspider <arturo.itiguala@gmail.com>
2021-03-18 15:38:37 -0600

    jetstream installation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,26 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
     purge: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
         },
     },
 
     variants: {
-        opacity: ["disabled"],
-        screens: {
-            sm: "640px",
-            // => @media (min-width: 640px) { ... }
-
-            md: "768px",
-
-            lg: "1024px",
-            // => @media (min-width: 1024px) { ... }
-
-            xl: "1280px",
-            // => @media (min-width: 1280px) { ... }
-
-            "2xl": "1600px",
-            // => @media (min-width: 1600px) { ... }
-        },
-        colors: {
-            "light-back": "#fafafa",
-            "light-gray": "#f1f1f1",
-            "main-yellow": "#fdcb46",
-            "red-step": "#faf8f0",
-        },
         extend: {
-            opacity: ["disabled"],
+            opacity: ['disabled'],
         },
     },
 
-    plugins: [require("@tailwindcss/forms")],
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
 };
