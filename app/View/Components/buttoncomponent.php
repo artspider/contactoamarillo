aa5e06b8deb3faa1aaa57d4d05a2f347abb4e2a8
artspider <arturo.itiguala@gmail.com>
2021-03-19 21:03:14 -0600

    Capitalize Class for component Button

--- app/View/Components/buttoncomponent.php
+++ app/View/Components/buttoncomponent.php
@@ -4,7 +4,7 @@ namespace App\View\Components;
 
 use Illuminate\View\Component;
 
-class buttoncomponent extends Component
+class Buttoncomponent extends Component
 {
     public $type;    
     public $alpine;
