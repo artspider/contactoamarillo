138f59c4b52d750d336de21181298afdc6ee0e33
Arturo rodriguez <arturo.itiguala@gmail.com>
2021-04-06 19:22:12 -0500

    start module dashboard

--- resources/js/alpine-functions.js
+++ resources/js/alpine-functions.js
@@ -1,158 +1,151 @@
 function menu() {
-    return {
-        show: false,
-        open: function () {
-            this.show = true;
-        },
-        close: function () {
-            this.show = false;
-        },
-        isOpen: function () {
-            return this.show === true;
-        },
-    };
+  return {
+    show: false,
+    open: function () {
+      this.show = true
+    },
+    close: function () {
+      this.show = false
+    },
+    isOpen: function () {
+      return this.show === true
+    },
+  }
 }
-window.menu = menu;
+window.menu = menu
 
 function togleWireClass(key, elementId) {
-    var tag = document.getElementById(elementId);
-    tag.classList.toggle("tagSelected");
+  var tag = document.getElementById(elementId)
+  tag.classList.toggle('tagSelected')
 }
-window.togleWireClass = togleWireClass;
+window.togleWireClass = togleWireClass
 
 function educationListen() {
-    return {
-        isEditing: false,
-        openEdit: function () {
-            this.isEditing = true;
-        },
-        closeEdit: function () {
-            this.isEditing = false;
-        },
-        deleteItem: function (id) {
-            confirmAction("eliminarcarrera", id);
-        },
-    };
+  return {
+    isEditing: false,
+    openEdit: function () {
+      this.isEditing = true
+    },
+    closeEdit: function () {
+      this.isEditing = false
+    },
+    deleteItem: function (id) {
+      confirmAction('eliminarcarrera', id)
+    },
+  }
 }
-window.educationListen = educationListen;
+window.educationListen = educationListen
 
 function languageListen() {
-    return {
-        deleteItem: function (id) {
-            confirmAction("refreshLanguage", id);
-        },
-    };
+  return {
+    deleteItem: function (id) {
+      confirmAction('refreshLanguage', id)
+    },
+  }
 }
-window.languageListen = languageListen;
+window.languageListen = languageListen
 
 function abilityListen() {
-    return {
-        tagId: null,
-        habilidades: true,
-        togleTagClass: function (key, event) {
-            console.log("en el togle tag class");
-            var tag = document.getElementById(event.target.id);
-            console.log(tag);
-            tag.classList.toggle("tagSelected");
-            Livewire.emit("onemoreability", key);
-        },
-        togleTagRemoveClass: function (key, event) {
-            console.log("en el remove tag class");
-            var tag = document.getElementById(event.target.id);
-            console.log(tag);
-            tag.classList.toggle("tagRemoveSelected");
-            Livewire.emit("onelessability", key);
-        },
-    };
+  return {
+    tagId: null,
+    habilidades: true,
+    togleTagClass: function (key, event) {
+      console.log('en el togle tag class')
+      var tag = document.getElementById(event.target.id)
+      console.log(tag)
+      tag.classList.toggle('tagSelected')
+      Livewire.emit('onemoreability', key)
+    },
+    togleTagRemoveClass: function (key, event) {
+      console.log('en el remove tag class')
+      var tag = document.getElementById(event.target.id)
+      console.log(tag)
+      tag.classList.toggle('tagRemoveSelected')
+      Livewire.emit('onelessability', key)
+    },
+  }
 }
-window.abilityListen = abilityListen;
+window.abilityListen = abilityListen
 
 function subareaListen() {
-    return {
-        tagId: null,
-        habilidades: true,
-        togleTagClass: function (key, event) {
-            console.log("en el togle tag class");
-            console.log(event);
-            var tag = document.getElementById(event.target.id);
-            console.log(tag);
-            tag.classList.toggle("tagSelected");
-            Livewire.emit("toggleSubareaModel", key);
-        },
-        togleTagRemoveClass: function (key, event) {
-            console.log("en el remove tag class");
-            var tag = document.getElementById(event.target.id);
-            console.log(tag);
-            tag.classList.toggle("tagRemoveSelected");
-            Livewire.emit("onelessability", key);
-        },
-    };
+  return {
+    tagId: null,
+    habilidades: true,
+    togleTagClass: function (key, event) {
+      console.log('en el togle tag class')
+      console.log(event)
+      var tag = document.getElementById(event.target.id)
+      console.log(tag)
+      tag.classList.toggle('tagSelected')
+      Livewire.emit('toggleSubareaModel', key)
+    },
+    togleTagRemoveClass: function (key, event) {
+      console.log('en el remove tag class')
+      var tag = document.getElementById(event.target.id)
+      console.log(tag)
+      tag.classList.toggle('tagRemoveSelected')
+      Livewire.emit('onelessability', key)
+    },
+  }
 }
-window.subareaListen = subareaListen;
+window.subareaListen = subareaListen
 
 function ServiceTagListen() {
-    return {
-        tagId: null,
-        habilidades: true,
-        togleTagClass: function (key, event) {
-            console.log("en el togle tag class");
-            var tag = document.getElementById(event.target.id);
-            console.log(tag);
-            tag.classList.toggle("tagSelected");
-            Livewire.emit("toggletag", key);
-        },
-    };
+  return {
+    tagId: null,
+    habilidades: true,
+    togleTagClass: function (key, event) {
+      console.log('en el togle tag class')
+      var tag = document.getElementById(event.target.id)
+      console.log(tag)
+      tag.classList.toggle('tagSelected')
+      Livewire.emit('toggletag', key)
+    },
+  }
 }
-window.ServiceTagListen = ServiceTagListen;
+window.ServiceTagListen = ServiceTagListen
 
-<<<<<<< HEAD
 function CertificationListen() {
-    return {
-        tagId: null,
-        habilidades: true,
-        prueba: function (key, event) {
-            alert("en el CertificationListen");
-            
-        },
-    };
+  return {
+    tagId: null,
+    habilidades: true,
+    prueba: function (key, event) {
+      alert('en el CertificationListen')
+    },
+  }
 }
-window.CertificationListen = CertificationListen;
+window.CertificationListen = CertificationListen
 
 function descripcion() {
-    return {
-        quillShow: function() {
-            
-            var delta = quill.container.firstChild.innerHTML;
-            console.log(delta);
-            Livewire.emit("servicedescription", delta);
-        }
-    };
+  return {
+    quillShow: function () {
+      var delta = quill.container.firstChild.innerHTML
+      console.log(delta)
+      Livewire.emit('servicedescription', delta)
+    },
+  }
 }
-window.descripcion = descripcion;
-=======
-function input(){
-    return {
-        open: false,
-        focusInput: function(event){
-            var inputSear = event.target;
-            console.log(inputSear);
-            if(inputSear.classList.contains('inactivo')){
-                inputSear.classList.remove('inactivo');
-                inputSear.classList.add('activo');
-            }
-            console.log('Diste click adentro');
-        },
-        focusLost: function(event){
-            var inputSear = event.target;
-            console.log(inputSear);
-            if(inputSear.classList.contains('activo')){
-                inputSear.classList.remove('activo');
-                inputSear.classList.add('inactivo');
-            }
-            
-        }
-    }
+window.descripcion = descripcion
+function input() {
+  return {
+    open: false,
+    focusInput: function (event) {
+      var inputSear = event.target
+      console.log(inputSear)
+      if (inputSear.classList.contains('inactivo')) {
+        inputSear.classList.remove('inactivo')
+        inputSear.classList.add('activo')
+      }
+      console.log('Diste click adentro')
+    },
+    focusLost: function (event) {
+      var inputSear = event.target
+      console.log(inputSear)
+      if (inputSear.classList.contains('activo')) {
+        inputSear.classList.remove('activo')
+        inputSear.classList.add('inactivo')
+      }
+    },
+  }
 }
-window.input = input;
-
->>>>>>> d57ed6fd1a89e7039ef5e8ee66debd85035c5468
+window.input = input
--- resources/views/components/contacto-amarillo/contacto-dashboard.blade.php
+++ resources/views/components/contacto-amarillo/contacto-dashboard.blade.php
@@ -11,165 +11,166 @@
 
     <!-- Aquì va menuside -->
 
-        
-            <div class="lg:flex lg:justify-center xl:justify-end">
-                <x-menuside>
-                </x-menuside>
-                <x-dashboard-n>
-                    <x-overview >
-                            <x-usuarioMob>
-                                Hello,
-                                <x-slot name="nombre">
-                                    Carlos Carbajal
+
+    <div class="lg:flex lg:justify-center xl:justify-end">
+        {{-- <x-menuside>
+        </x-menuside> --}}
+        <x-dashboard-n>
+            <x-overview>
+                <x-usuarioMob>
+                    Hello,
+                    <x-slot name="nombre">
+                        Carlos Carbajal
+                    </x-slot>
+
+                    <x-slot name="sourc">
+                        https://randomuser.me/api/portraits/men/75.jpg
+                    </x-slot>
+                </x-usuarioMob>
+
+                <div class="lg:flex justify-around">
+                    <x-aboutYou>
+                        <x-earnings>
+                            <!-- Aqui deberia ir la una grafica por ahora esta como img -->
+                            <x-slot name="graph">
+                                https://cdn140.picsart.com/268891729036211.png?type=webp&to=min&r=640
+                            </x-slot>
+
+                            <x-slot name="money">
+                                8,900
+                            </x-slot>
+
+                            <x-slot name="improvement">
+                                +10 since last month
+                            </x-slot>
+                        </x-earnings>
+
+                        <x-containerRank>
+                            <x-rankUser>
+                                <x-slot name="numberRank">
+                                    98
                                 </x-slot>
 
-                                <x-slot name="sourc">
-                                    https://randomuser.me/api/portraits/men/75.jpg
+                                <x-slot name="topPercentage">
+                                    In top 30%
                                 </x-slot>
-                            </x-usuarioMob>
+                            </x-rankUser>
 
-                            <div class="lg:flex justify-around">    
-                                <x-aboutYou>
-                                    <x-earnings>
-                                        <!-- Aqui deberia ir la una grafica por ahora esta como img -->
-                                        <x-slot name="graph">
-                                            https://cdn140.picsart.com/268891729036211.png?type=webp&to=min&r=640
-                                        </x-slot>
+                            <x-projectsLeft>
+                                <x-slot name="numberProjects">
+                                    31
+                                </x-slot>
 
-                                        <x-slot name="money">
-                                            8,900
-                                        </x-slot>
+                                <x-slot name="projectsFinished">
+                                    8 this month
+                                </x-slot>
 
-                                        <x-slot name="improvement">
-                                            +10 since last month
-                                        </x-slot>
-                                    </x-earnings>
+                                <x-slot name="advantage1">
+                                    mobile app
+                                </x-slot>
 
-                                    <x-containerRank>
-                                        <x-rankUser>
-                                            <x-slot name="numberRank">
-                                                98
-                                            </x-slot>
+                                <x-slot name="advantage2">
+                                    branding
+                                </x-slot>
+                            </x-projectsLeft>
 
-                                            <x-slot name="topPercentage">
-                                                In top 30%
-                                            </x-slot>
-                                        </x-rankUser>
+                        </x-containerRank>
+                    </x-aboutYou>
 
-                                        <x-projectsLeft>
-                                            <x-slot name="numberProjects">
-                                                31
-                                            </x-slot>
+                    <x-yourProjects>
+                        <x-inProgressProject>
+                            <x-slot name="srcImg">
+                                https://randomuser.me/api/portraits/women/2.jpg
+                            </x-slot>
 
-                                            <x-slot name="projectsFinished">
-                                                8 this month
-                                            </x-slot>
+                            <x-slot name="typeProject">
+                                Logo design for Bakery
+                            </x-slot>
 
-                                            <x-slot name="advantage1">
-                                                mobile app
-                                            </x-slot>
+                            <x-slot name="daysRemaing">
+                                1 day remaing
+                            </x-slot>
 
-                                            <x-slot name="advantage2">
-                                                branding
-                                            </x-slot>
-                                        </x-projectsLeft>
+                        </x-inProgressProject>
 
-                                    </x-containerRank>
-                                </x-aboutYou>
+                        <x-inProgressProject>
+                            <x-slot name="srcImg">
+                                https://randomuser.me/api/portraits/men/41.jpg
+                            </x-slot>
 
-                                <x-yourProjects>
-                                    <x-inProgressProject>
-                                        <x-slot name="srcImg">
-                                            https://randomuser.me/api/portraits/women/2.jpg
-                                        </x-slot>
+                            <x-slot name="typeProject">
+                                Personal branding project
+                            </x-slot>
 
-                                        <x-slot name="typeProject">
-                                            Logo design for Bakery
-                                        </x-slot>
+                            <x-slot name="daysRemaing">
+                                5 days remaing
+                            </x-slot>
+                        </x-inProgressProject>
 
-                                        <x-slot name="daysRemaing">
-                                            1 day remaing
-                                        </x-slot>
+                        <x-slot name="pathProjects">
+                            #
+                        </x-slot>
+                    </x-yourProjects>
+                </div>
 
-                                    </x-inProgressProject>
-                                        
-                                    <x-inProgressProject>
-                                        <x-slot name="srcImg">
-                                            https://randomuser.me/api/portraits/men/41.jpg
-                                        </x-slot>
+                <div class="lg:flex justify-around">
+                    <x-recommendedProjects>
+                        <x-slot name="nameAutorProject">
+                            Thomas Martin
+                        </x-slot>
+                        <x-slot name="lastUpdate">
+                            Updated 10m ago
+                        </x-slot>
+                        <x-slot name="typeProject">
+                            Design
+                        </x-slot>
+                        <x-slot name="topicProject">
+                            Need a designer to form branding essentials for my business.
+                        </x-slot>
+                        <x-slot name="descriptionProject">
+                            Looking for a talented brand designer to create all the branding materials for my new
+                            startup
+                        </x-slot>
+                        <x-slot name="priceProject">
+                            8,350
+                        </x-slot>
+                        <x-slot name="typeOfPay">
+                            month
+                        </x-slot>
+                        <x-slot name="hiring">
+                            Full time
+                        </x-slot>
+                    </x-recommendedProjects>
 
-                                        <x-slot name="typeProject">
-                                            Personal branding project
-                                        </x-slot>
+                    <x-invoices>
+                        <x-paymentsMade>
+                            <x-slot name="srcImg">
+                                https://randomuser.me/api/portraits/women/8.jpg
+                            </x-slot>
+                        </x-paymentsMade>
 
-                                        <x-slot name="daysRemaing">
-                                            5 days remaing
-                                        </x-slot>
-                                    </x-inProgressProject>
-
-                                    <x-slot name="pathProjects">
-                                        #
-                                    </x-slot>
-                                </x-yourProjects>
-                            </div>
-
-                            <div class="lg:flex justify-around"> 
-                                <x-recommendedProjects>
-                                    <x-slot name="nameAutorProject">
-                                            Thomas Martin
-                                    </x-slot>
-                                    <x-slot name="lastUpdate">
-                                            Updated 10m ago
-                                    </x-slot>
-                                    <x-slot name="typeProject">
-                                            Design
-                                    </x-slot>
-                                    <x-slot name="topicProject">
-                                        Need a designer to form branding essentials for my business.
-                                    </x-slot>
-                                    <x-slot name="descriptionProject">
-                                        Looking for a talented brand designer to create all the branding materials for my new startup 
-                                    </x-slot>
-                                    <x-slot name="priceProject">
-                                        8,350 
-                                    </x-slot>
-                                    <x-slot name="typeOfPay">
-                                        month
-                                    </x-slot>
-                                    <x-slot name="hiring">
-                                        Full time
-                                    </x-slot>
-                                </x-recommendedProjects>
-
-                                <x-invoices>
-                                    <x-paymentsMade>
-                                        <x-slot name="srcImg">
-                                            https://randomuser.me/api/portraits/women/8.jpg
-                                        </x-slot>
-                                    </x-paymentsMade>
-
-                                    <x-paymentsMade>
-                                        <x-slot name="srcImg">
-                                            https://randomuser.me/api/portraits/men/8.jpg
-                                        </x-slot>
-                                    </x-paymentsMade>
-                                </x-invoices>
-                            </div>
-                    </x-overview>
-                </x-dashboard-n>
-            </div>
+                        <x-paymentsMade>
+                            <x-slot name="srcImg">
+                                https://randomuser.me/api/portraits/men/8.jpg
+                            </x-slot>
+                        </x-paymentsMade>
+                    </x-invoices>
+                </div>
+            </x-overview>
+        </x-dashboard-n>
+    </div>
 
 
-        <!-- Pero creo que aqui deberia de ir cada menuItemUser -->
+    <!-- Pero creo que aqui deberia de ir cada menuItemUser -->
 
 
-        <!-- Y cada menuItem NORMAL -->
-        <!-- Y cada menuItem NORMAL -->
-        <!-- Y cada menuItem NORMAL -->
-        <!-- Y cada menuItem NORMAL -->
-        <!-- Y cada menuItem NORMAL -->
-        <!-- Y cada menuItem NORMAL -->
-        <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
+    <!-- Y cada menuItem NORMAL -->
 
     @push('modals')
     <script>
