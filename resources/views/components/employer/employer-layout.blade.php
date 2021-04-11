6f95fddfb2fd102065d82b112771ecd6e6baa06f
Juan Carlos Carbajal Salgado <68079881+CarlosCarbajalDev@users.noreply.github.com>
2021-04-10 23:30:59 -0500

    agregué las categorías

--- resources/views/components/employer/employer-layout.blade.php
+++ resources/views/components/employer/employer-layout.blade.php
@@ -63,13 +63,15 @@
                         <x-employer.section>
                             <h2 class="font-bold text-2xl mt-8 mb-5">Servicios populares</h2>
                                 <x-employer.sliderWrapper>
+                                    
+                                    
                                     <x-employer.sliderItem2>
                                         <x-slot name="imgSource">
                                             https://images.unsplash.com/flagged/photo-1574857127280-f70bf547a03e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80
                                         </x-slot>
 
                                         <x-slot name="jobName">
-                                            Photographer
+                                            Fotografía
                                         </x-slot>
                                     </x-employer.sliderItem>
 
@@ -89,7 +91,7 @@
                                         </x-slot>
 
                                         <x-slot name="jobName">
-                                            Designer
+                                            Diseño gráfico
                                         </x-slot>
                                     </x-employer.sliderItem>  
 
@@ -109,7 +111,17 @@
                                         </x-slot>
 
                                         <x-slot name="jobName">
-                                            Singer
+                                           Música y audio 
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1563986768494-4dee2763ff3f?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8c29jaWFsJTIwbWVkaWF8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60
+                                        </x-slot>
+
+                                        <x-slot name="jobName">
+                                            Redes sociales y publicidad web
                                         </x-slot>
                                     </x-employer.sliderItem>
 
@@ -122,7 +134,127 @@
                                             Cartoonist
                                         </x-slot>
                                     </x-employer.sliderItem>
-                            </x-employer.sliderWrapper>
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1517245386807-bb43f82c33c4?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=750&q=80
+                                        </x-slot>
+
+                                        <x-slot name="jobName">
+                                            Asesoría para empresas
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1581094480465-4e6c25fb4a52?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
+                                        </x-slot>
+
+                                        <x-slot name="jobName">
+                                            Ingeniría y arquitectura
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1546514714-df0ccc50d7bf?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=667&q=80
+                                        </x-slot>
+
+                                        <x-slot name="jobName">
+                                            Ventas freelancer
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1517048676732-d65bc937f952?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
+                                        </x-slot>
+
+                                        <x-slot name="jobName">
+                                            Marketing y publicidad
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1599999905123-b5e8ca9c91b5?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Soporte administrativo
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1555436169-20e93ea9a7ff?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Enseñanza
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1573497490701-f84eda04e280?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Diseño editorial
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1616267626443-4e0e6adcaf11?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Vídeo y animación
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1503387837-b154d5074bd2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=889&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Redación y edición de textos
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1612776572997-76cc42e058c3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=625&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Investigación
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+
+                                    <x-employer.sliderItem2>
+                                        <x-slot name="imgSource">
+                                            https://images.unsplash.com/photo-1576267421707-b255b46fc4fb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80
+                                        </x-slot>
+                                            
+                                        <x-slot name="jobName">
+                                            Traducciones
+                                        </x-slot>
+                                    </x-employer.sliderItem>
+
+                                </x-employer.sliderWrapper>
                         </x-employer.section>
                     </x-employer.container>
             </main>
