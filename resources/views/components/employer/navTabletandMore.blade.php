7c6a1082a245c8746f8ce983e84adf5f3b269399
Arturo rodriguez <arturo.itiguala@gmail.com>
2021-04-18 18:33:07 -0500

    change dashboard logout ok

--- resources/views/components/employer/navTabletandMore.blade.php
+++ resources/views/components/employer/navTabletandMore.blade.php
@@ -6,7 +6,19 @@
         <li class="hover:underline hover:text-main-yellow"><a href="/proyects">Proyectos</a></li>
         <li class="hover:underline hover:text-main-yellow"><a href="/search-expert">Buscar experto</a></li>
         <li class="hover:underline hover:text-main-yellow"><a href="/account">Cuenta</a></li>
-        <li class="border-white border px-3 py-2 rounded-md hover:bg-white hover:text-black"><a
-                href="/logout">Logout</a></li>
+        <li class="border-white border px-3 py-2 rounded-md hover:bg-white hover:text-black">
+            <!-- Logout -->
+
+
+            <form method="POST" action="{{ route('logout') }}">
+                @csrf
+                <a class="flex items-center focus:outline-none duration-150 ease-in-out" href="{{ route('logout') }}"
+                    onclick="event.preventDefault(); this.closest('form').submit();">
+
+                    <p>Cerrar sesión</p>
+                </a>
+            </form>
+
+        </li>
     </ul>
 </div>
