b43c01ac54b4e2cac66111e0f7b42feff23eb6fd
Juan Carlos Carbajal Salgado <68079881+CarlosCarbajalDev@users.noreply.github.com>
2021-04-26 14:27:31 -0500

    Version mobil terminada

--- resources/views/components/employer/formSearchEmployer.blade.php
+++ resources/views/components/employer/formSearchEmployer.blade.php
@@ -1,5 +1,9 @@
-<form action="#" class="flex flex-col py-3 h-432 pt-10 justify-between">
-    <h1 class="text-white text-lg">Hola, {{$nameUser}}, ¿qué perfil estas buscando hoy?</h1>
-    {{$slot}}
-    <input type="submit" value="Buscar" class="bg-main-yellow text-black text-2xl w-full h-14 rounded-2xl font-semibold">
+<form action="#" class="flex flex-col  h-432 pt-12 justify-between">
+    <h1 class="text-white text-lg text-center">Hola, {{$nameUser}}, ¿qué perfil estas buscando hoy?</h1>
+    <div class="h-72 flex flex-col justify-between">
+        <div class="">
+            {{$slot}}
+        </div><!-- Contiene los 2 inputSearch -->
+        <input type="submit" value="Buscar" class="bg-main-yellow text-black text-2xl w-full h-14 rounded-2xl font-semibold cursor-pointer hover:bg-yellow-500">
+    </div><!-- Parte abajo -->
 </form>
--- resources/views/components/employer/inputSearchEmployer.blade.php
+++ resources/views/components/employer/inputSearchEmployer.blade.php
@@ -1,7 +1,11 @@
-<div class="block">
-    <label for="skill" class="text-white text-sm">Perfil</label>
-    <div class="relative">
-        <input type="text" id="skill" placeholder="Test" class="bg-white w-full h-14 rounded-2xl absolute pl-12 pb-3">
-        <svg class="absolute top-4 left-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M20.822 18.096c-3.439-.794-6.641-1.49-5.09-4.418 4.719-8.912 1.251-13.678-3.732-13.678-5.081 0-8.464 4.949-3.732 13.678 1.597 2.945-1.725 3.641-5.09 4.418-2.979.688-3.178 2.143-3.178 4.663l.005 1.241h10.483l.704-3h1.615l.704 3h10.483l.005-1.241c.001-2.52-.198-3.975-3.177-4.663zm-8.231 1.904h-1.164l-.91-2h2.994l-.92 2z"/></svg>
+<div class="h-auto mb-5">
+    <label for="{{$labelId}}" class="text-white text-sm">{{$label}}</label>
+    <div class="relative mt-1 h-auto">
+        <input type="text" id="{{$labelId}}" placeholder="{{$placeholder}}" class="bg-white w-full h-14 rounded-2xl  pl-14">
+        <div class="absolute top-4 left-4">
+            {{$imgInput}}
+        </div>
     </div>
 </div>
+
+
